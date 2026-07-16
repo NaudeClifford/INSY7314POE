f3ba5614f3ab64c55e5b86b09d47dcdbf88a17de
Naude-Clifford <cliffordnaude990@gmail.com>
2026-07-16 14:16:41 +0200

    Initialize the Project

---
+++ INSY7314POE/INSY7314MVC/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using INSY7314MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace INSY7314MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
